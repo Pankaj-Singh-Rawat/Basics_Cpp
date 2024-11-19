53df4f5a241e990a35128a3084e703c6ec8cd3b3
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-19 23:50:35 +0530

    printing a line without using the namespace library

---
+++ intro.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // header file library , let's us work with I/O Objects like cout
+using namespace std; // we can use names for variables and objects\
+
+int main(){ // main is the function , code inside this will be executed
+    cout << "Hello World."; // cout is an object and << ( is output ) , it will output "whatever is inside here".
+    return 0 ; // return is ending the main function here and, ; -> this is used to terminate a line
+}
---
+++ omitting_namespace.cpp
@@ -0,0 +1,11 @@
+// how will have to perform  operation of we do not use 'namsespace std' in the code??
+
+#include <iostream>
+
+int main(){
+    std:: cout << "Hello , my name is Pankaj Singh Rawat";
+    return 0;
+}
+
+// it is entirely upto you if you want to use a namespace lirary or not !
+// but using the library is recommended.
