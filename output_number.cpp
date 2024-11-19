8e0eae07af40d262bb978a6f8750204da01a24f1
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-20 00:21:16 +0530

    output numbers and endline statements

---
+++ output_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 3 ; // numbers can be printed directly
+    cout << "\n";  // \n -> prints next line
+    cout << 3 + 3 ; // or simple mathmatical calculations can be done like this itself
+
+    cout << "\n";
+
+    cout << "hi boys!" <<"\n\n"; // two \n -> creates a blank line
+
+    cout << "how is there a blank line above?" << endl;
+
+    cout << "a new line can also be created using 'endl' at the end of statement" << endl;
+
+    cout << "that's how it's done";
+
+}
