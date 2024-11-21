635159e23d4bca1fcc7e1c3dfbbc28269038409e
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-21 18:39:01 +0530

    real life example of using variables

---
+++ variable_examples.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // real like examples of variales
+    int studentID = 1111;
+    int studentAge = 20 ;
+    float studentFee = 75.5 ;
+    char studentGrade = 'B' ;
+
+    // print variables
+    cout << "Student ID : " << studentID << "\n";
+    cout << "Student Age : " << studentAge << "\n";
+    cout << "Student Fee : " << studentFee << "\n";
+    cout << "Student Grade : " << studentGrade << "\n";
+
+    // calculate the area of a rectangle
+    int length = 4; 
+    int width = 5;
+    int area ;
+
+    area = length * width;
+
+    cout << "Area of the retangle is : " << area << endl;
+
+}
