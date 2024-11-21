870088fdeb5cb6088e2a682334fe8d0327436cd9
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-21 18:30:54 +0530

    what are identifiers and how are constants used

---
+++ identifiers_&_constants.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    // identifiers are the names we assign to the variables 
+    // by the name of the identifier we can understand what the variable stands for
+
+    int minutesPerHour = 60 ; // this is a good example of identifiers
+    int m = 60 ;// this is a bad example of identifiers
+
+    // CONSTANTS : defining a contstant
+    const int myNum = 15 ; // myNum will always be 15
+    // what if I reassign the value of myNum ??
+
+    // myNum = 10 ; // this throws an error
+
+    // const float PI;
+    // Pi = 3.14; // we can not assign the value of constant like this.
+
+}
