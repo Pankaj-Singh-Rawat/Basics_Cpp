27ee95cf93ebb67e0b5e13935cb0d7da14743982
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-22 23:03:52 +0530

    How to take input from user

---
+++ user_input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    // today we will see how we can take input from the user !
+    // int x = 0 ;
+    // cout << "type a number: " ;
+    // cin >> x; // cin: takes number from the user 
+    // cout << "number you've entered is : " << x ; // prints the number entered by the user
+
+    // we can also do arithmatic operation like this :
+    int a , b ;
+    cout << "enter number A : " ; 
+    cin >> a ;
+    cout << "enter number B : " ;
+    cin >> b ;
+    int sum = a + b ;
+    cout << "sum of both numbers is : " << sum ;
+    
+}
