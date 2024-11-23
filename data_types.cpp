faa2993c4e2cd118da496b5f006555c3ddd38e21
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-23 23:56:14 +0530

    data types in C++

---
+++ data_types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // here are all different data types used in C++
+
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99;     // Floating point number
+double myDoubleNum = 9.98;   // Floating point number
+char myLetter = 'D';         // Character
+bool myBoolean = true;       // Boolean
+string myText = "Hello";     // String
+
+}
