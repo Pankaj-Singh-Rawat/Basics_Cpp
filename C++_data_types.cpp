ef4e8f3ef0599538a6a2e0aa8084b5a42e45c83d
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-24 22:25:10 +0530

    usecase of some data types

---
+++ C++_data_types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main(){
+
+// we can do things like this to indicate the power of 10 
+
+float f1 = 35e3;
+double d1 = 12E4;
+cout << f1 << endl;
+cout << d1 << "\n\n"; 
+
+// boolean : returns a value in terms of true = 1 and false = 0 only 
+
+bool isCodingFun = true;
+bool isFishTasty = false;
+cout << isCodingFun << endl;  // Outputs 1 (true)
+cout << isFishTasty << "\n\n";  // Outputs 0 (false)
+
+// String : to use string you must input a header file in source code (2nd line of the code)
+
+    string name = "Pankaj Singh Rawat";
+    cout << name ;
+
+
+}
