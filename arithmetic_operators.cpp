14441236d33414e33860757f39679f03a796b1bc
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-25 21:17:21 +0530

    Arithmatic operators in C++

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // there are different types of opereators in C
+    // one of which is ARITHMATIC OPERATOR , which consists of :
+
+    // 1: Addition
+
+    int a = 2 ; 
+    int b = 5 ;
+
+    int add = a + b ;
+    cout << "Addition is : " << add << endl ;
+
+    // 2: Subtraction
+
+    int sub = a - b ;
+    cout << "Subtraction is : " << sub << endl ;
+
+    // 3. Multiplication
+
+    int product = a * b ;
+    cout << "Product is : " << product << endl ;
+
+    // 4. Division
+
+    int div = b / a ;
+    cout << "division is : " << div << endl ;
+
+    // 5. Modulus
+
+    int modulus = b % a ;
+    cout << "Modulus is : " << modulus << endl ;
+
+    // 6. Increment
+
+    cout << "Increment of 'a' is : " << a++ << endl ;
+
+    // 7. decrement
+
+    cout << "Decrement of 'b' is : " << b-- << endl ;
+
+
+
+}
