5e7a9653b082d90c0cda339e39cbc611f5ddf215
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-25 21:25:06 +0530

    Assignment operators in C++

---
+++ assignment_operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    // Assignment operators
+
+    Operator	Example	    Same As	
+    =	        x = 5	    x = 5	
+    +=	        x += 3	    x = x + 3	
+    -=	        x -= 3	    x = x - 3	
+    *=	        x *= 3	    x = x * 3	
+    /=	        x /= 3	    x = x / 3	
+    %=	        x %= 3	    x = x % 3	
+    &=	        x &= 3	    x = x & 3	
+    |=	        x |= 3	    x = x | 3	
+    ^=	        x ^= 3	    x = x ^ 3	
+    >>=	        x >>= 3	    x = x >> 3	
+    <<=	        x <<= 3	    x = x << 3  
+
+    // you can try them all in any operation you would like them to use
+}
