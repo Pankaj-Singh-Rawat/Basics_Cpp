0fb0cd9e117ab693f840ec34058735ad0229874c
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-26 21:22:38 +0530

    a list of comparison operators

---
+++ comparison_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+//     A list of all comparison operators:
+    
+// Operator	Name	                    Example	
+//     ==	    Equal to	                x == y	
+//     !=	    Not equal	                x != y	
+//     >	    Greater than	            x > y	
+//     <	    Less than	                x < y	
+//     >=	    Greater than or equal to	x >= y	
+//     <=	    Less than or equal to	    x <= y
+
+}
