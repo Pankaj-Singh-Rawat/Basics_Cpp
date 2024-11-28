96b80e0be4fbe13efc68e7b92caf1c8673522ae1
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-28 22:34:59 +0530

    String concatenation Append type

--- Strings/string_concatenation.cpp
+++ Strings/string_concatenation.cpp
@@ -10,6 +10,18 @@ int main(){
     string lastName = "Rawat";
 
     string fullName = firstName + " " + middleName + " " + lastName ; //here + is use do join two string with spaces using ->" "
-    cout << fullName;
+    cout << fullName << endl;
+
+    // A funcion called append can also be used to add up two strings such as :
+
+    // string fullName2 = firstName.append(middleName); // now the firstname = "PankajSingh" istead of "Pankaj"
+    // cout << fullName2 <<endl ;
+
+    // or nested(functions inside a function) functions can also be used such as :
+
+    string fullname3 = firstName.append(middleName.append(lastName)); // middleName is added with last name and then last name is added with the firstname;
+    cout << firstName << endl;
+
+    
 
 }
