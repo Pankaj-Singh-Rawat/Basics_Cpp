4caf8567aec9cbe373eaff7bd4e42df8c5d263f3
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-28 22:15:48 +0530

    string concatenation

---
+++ Strings/string_concatenation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+    // concatenation : adding two strings via '+' operator is called concatenation
+
+    string firstName = "Pankaj";
+    string middleName = "Singh";
+    string lastName = "Rawat";
+
+    string fullName = firstName + " " + middleName + " " + lastName ; //here + is use do join two string with spaces using ->" "
+    cout << fullName;
+
+}
