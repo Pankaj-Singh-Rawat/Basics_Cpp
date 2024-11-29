07f506edfbde25f080b6edc3d6012ddc48b32fa0
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-11-29 20:02:13 +0530

    numbers and strings addition

---
+++ Strings/numbers_strings.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+    int x = 10 ; 
+    int y = 20 ;
+    int z = x + y ;
+
+    cout << z << endl ;
+
+// addition of numbers through strings ;
+
+    string a = "10";
+    string b = "20";
+    string c = a + b ; // adding strings : z = 1020 "binary addition"
+
+    cout << c << endl ;
+
+// addition of a number in string and a number - an error will occur then;
+
+    string p = "10";
+    int q = 20 ;
+    string r = p + q ;
+
+}
