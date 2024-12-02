acdc7741acf71cc5206b2344c6ee3924ac3c9c4e
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-02 18:27:03 +0530

    C-style strings

---
+++ Strings/Cstylestrings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+
+    string greeting1 = "Hello"; // regular string
+    char greeting2[] = "Hello"; // C-style string (an array of characters)
+
+    cout << greeting1 << endl ;
+    cout << greeting2 << endl ;
+    
+}
