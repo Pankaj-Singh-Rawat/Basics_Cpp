6255af82353c56da88b9926e8ba4d325c1270b89
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-03 22:57:31 +0530

    Boolean

---
+++ boolean/boolean_values.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    bool amIhuman = true ;
+    bool amInotHuman = false ;
+    cout << amIhuman << endl; // output : 1 
+    cout << amInotHuman << endl; // output : 0 
+}
