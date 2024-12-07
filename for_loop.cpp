ddbe3374f6e2babb6543a2bd68dadd0828c61ce9
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-07 22:33:15 +0530

    using for loop

---
+++ for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // when you exactly know how many times you want to use loop , for loop is used 
+    for(int i = 1 ; i <= 10 ; i += 2){
+        cout << i << endl ;
+    }
+
+    
+}
