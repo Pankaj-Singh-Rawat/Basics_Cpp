4479d647b2454f99c5349e41f3a71047087d9ea0
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-08 23:10:21 +0530

    break statement use

---
+++ break_continue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+
+    // break : when used break out of any loop or condition
+    for ( int i = 0 ; i <= 10 ; i++ ){
+        cout << i << endl ;
+        if( i == 4){
+            break;
+        }
+    }
+
+    
+}
