4817e57ae73ff20cb9d38849039a60a00c7f41b6
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-12 00:04:46 +0530

    for loop in arrays

---
+++ Array/arrays_and_loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // using for loops in arrays
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+
+    // Loop through strings
+    for (int i = 0; i < 5; i++) {
+        cout << cars[i] << "\n";
+    }
+
+    
+}
