def4bb63e02c1cdfd40a7049f4bf1623e895e973
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-17 23:37:20 +0530

    using one stricture for multiple variables

--- structures.cpp
+++ structures.cpp
@@ -15,5 +15,27 @@ int main (){
     cout << myStructure.myNum << endl ;
     cout << myStructure.myString << endl;
 
+    // using one structure in multiple variables 
+    struct{
+        int myNum ;
+        string myString ;
+    } mystruct1 , mystruct2 , mystruct3 ; // multiple structure variales seperated with commas 
 
+    struct {
+        string brand ;
+        string model ;
+        int year ;
+    } myCar1 , myCar2 ;
+
+    myCar1.brand = "BMW";
+    myCar1.model = "M5 Competition";
+    myCar1.year = 2023 ;
+
+    myCar2.brand = "Mustang";
+    myCar2.model = "GT";
+    myCar2.year = 2021;
+    cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << endl ; 
+    cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << endl ; 
+
+    
 }
