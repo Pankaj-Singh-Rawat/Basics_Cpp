f2d96467a9ccfd385c231692aaa0a267503f7195
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-26 22:41:52 +0530

    how to use functions

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+// creating a function
+void myFunction(){
+    //code here
+    cout << "Hello Function" ;
+}
+
+int main (){
+    // calling the function:
+    myFunction();
+    
+}
+
