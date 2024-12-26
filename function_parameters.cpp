ea1f815ad07b61bd84d82333630930168fa684ee
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-26 22:58:30 +0530

    passing default value in functions

--- function_parameters.cpp
+++ function_parameters.cpp
@@ -5,8 +5,18 @@ void functionName(string name){
         cout << name << endl ;
     }
 
+    //default parameter
+void defaultFunctions(string country = "INDIA"){
+    cout << country << endl;
+}
+
 int main(){
     functionName("Pankaj");
     functionName("Singh");
     functionName("Rawat");
+
+    defaultFunctions("Russia");
+    defaultFunctions("Nepal");
+    defaultFunctions("China");
 }
+
