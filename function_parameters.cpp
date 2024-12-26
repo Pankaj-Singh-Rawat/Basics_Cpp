58519064b5263eafb324bd7ff65da9fb40439a08
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-12-26 22:49:10 +0530

    passing parameters in functions

---
+++ function_parameters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+
+void functionName(string name){
+        cout << name << endl ;
+    }
+
+int main(){
+    functionName("Pankaj");
+    functionName("Singh");
+    functionName("Rawat");
+}
